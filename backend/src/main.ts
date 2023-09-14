71c0f87e73293f5b728b8134ec4c5de6a88ecebc
wanghao1993 <whao53333@gmail.com>
2023-09-14 23:32:55 +0800

    feat: 添加swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { HttpExceptionFilter } from './my-http-exception-filter/my-http-exception-filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const options = new DocumentBuilder()
+    .setTitle('通知系统')
+    .setDescription('自定通知系统后台')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
