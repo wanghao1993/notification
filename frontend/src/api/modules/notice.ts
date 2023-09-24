ce1b34532729bcf539f7f5cf3157fee07ed16484
wanghao1993 <whao53333@gmail.com>
2023-09-24 15:33:37 +0800

    feat: frontend

--- frontend/src/api/modules/notice.ts
+++
@@ -1,8 +0,0 @@
-import { NoticeTypes } from '@/types/notice.types';
-import { dfHttp } from '..';
-
-export const getNoticeListApi = (params: NoticeTypes.getNoticeListModal) => {
-  return dfHttp.get<string>('notice/list', {
-    params,
-  });
-};
--- frontend/src/types/notice.types.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export declare namespace NoticeTypes {
-  interface getNoticeListModal {
-    page: number;
-    pageSize: number;
-    keyword?: string;
-  }
-}
